3df83dc118a8af1144cd2c3188acc835cf47b1db
Alex Baciu <baciu6657@gmail.com>
2023-06-01 10:42:49 +0300

    NewPost links

--- client/src/pages/NewPost/NewPost.jsx
+++ client/src/pages/NewPost/NewPost.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Leftbar from "../../components/NewPostLeftbar/NewPostLeftbar";
 const NewPost = () => {
     return (
+            <div className="grid grid-cols-1 md:grid-cols-4">
+                <div className="px-6">
+                    <Leftbar />
+                </div>
             <div>
                 New Post
+            </div>
             </div>
         );
 };
