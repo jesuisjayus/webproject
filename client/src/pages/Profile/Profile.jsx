81767bda159b0a95297d24171108321dc58afa2b
Alex Baciu <baciu6657@gmail.com>
2023-06-02 09:50:05 +0300

    revert

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Leftbar from "../../components/ProfileLeftbar/ProfileLeftbar"; 
+import Navbar from "../../components/Navbar/Navbar";
 
 const Profile = () => {
     return (
+        <>
+            <div>
+                <Navbar />
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-4">
                 <div className="px-6">
                     <Leftbar />
@@ -11,6 +16,7 @@ const Profile = () => {
                     Profile 
                 </div>
             </div>
+        </>
         );
 };
 
