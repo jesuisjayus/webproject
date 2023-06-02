95a65349783e3e5995c93cbdba71fc0d33ec605d
augus <augustin.mons@insa-lyon.fr>
2023-06-02 09:11:57 +0200

    Mise en place de Redux

---
+++ client/src/redux/userSlice.js
@@ -0,0 +1,35 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    currentUser: null,,
+    isLoading: false,
+    error: false,
+};
+
+export const userSlice = createSlice({
+    name: "user",
+    intilisationState: initialState,
+    reducers: {
+        loginStart: (state) => {
+            state.isLoading = true;
+            state.error = false;
+        },
+        loginSuccess: (state, action) => {
+            state.isLoading = false;
+            state.currentUser = action.payload;
+        },
+        loginFailed : (state) => {
+            state.isLoading = false;
+            state.error = true;
+        },
+        logout:(state) => {
+            return initialState;
+        },
+    },
+});
+
+export const {loginStart, loginSuccess, loginFailed, logout} = userSlice.actions;
+
+
+
+export default userSlice.reducer;
