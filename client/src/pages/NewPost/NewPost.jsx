6da0bf84255144de4070657a833bb8b06b959f5a
jesuisjayus <aurelien.girin@gmail.com>
2023-06-02 08:59:22 +0200

    modf newpost

--- client/src/pages/NewPost/NewPost.jsx
+++ client/src/pages/NewPost/NewPost.jsx
@@ -16,26 +16,28 @@ const NewPost = () => {
         setCharacterCount2(text.length);
     };
     return (
-            //<div className="px-6">
-               // <Leftbar />
-            //</div>
-            <div>
-            <form className="bg-form flex flex-col px-8 py-12 rounded-lg w-8/12 md:w-6/12 mx-auto gap-10">
-                <h2 className="text-3xl text-text font-bold text-center">
-                    New Post
-                </h2>
-                <input type="text" placeholder="Title" className="text-xl px-4 py-2 rounded-full"/>
-                <ChooseCategory /> 
-                <textarea maxlength={280} title="max 280 characters" placeholder="Write a short description..." className="bg-blue-100 rounded-lg py-2 px-2" onChange={handleTextareaChange_short}></textarea>
-                <p className="text-text">Number of characters : {characterCount}/280</p>
-                <textarea maxlength={1000} title="max 1000 characters" placeholder="Write the full description..." className="bg-blue-100 rounded-lg py-2 px-2" onChange={handleTextareaChange_description}></textarea>
-                <p className="text-text">Number of characters : {characterCount2}/1000</p>
-                <button class="bg-button resize-none px-4 py-2 text-white rounded-full hover:bg-button-hover" style={{display: "flex", justifyContent:"center", alignItems:"flex-end"}}>
-                    Publish
-                </button>
-                
-                </form>
+            <div className="grid grid-cols-1 md:grid-cols-4">
+                <div className="px-6">
+                <Leftbar />
                 </div>
+                <div className="col-span-3 border-t-slate-800">
+                <form className="bg-form flex flex-col px-8 py-12 rounded-lg w-8/12 mx-0 gap-10">
+                    <h2 className="text-3xl text-text font-bold text-center">
+                        New Post
+                    </h2>
+                    <input type="text" placeholder="Title" className="text-xl px-4 py-2 rounded-full"/>
+                    <ChooseCategory /> 
+                    <textarea maxlength={280} title="max 280 characters" placeholder="Write a short description..." className="bg-blue-100 rounded-lg py-2 px-2" onChange={handleTextareaChange_short}></textarea>
+                    <p className="text-text">Number of characters : {characterCount}/280</p>
+                    <textarea maxlength={1000} title="max 1000 characters" placeholder="Write the full description..." className="bg-blue-100 rounded-lg py-2 px-2" onChange={handleTextareaChange_description}></textarea>
+                    <p className="text-text">Number of characters : {characterCount2}/1000</p>
+                    <button class="bg-button resize-none px-4 py-2 text-white rounded-full hover:bg-button-hover" style={{display: "flex", justifyContent:"center", alignItems:"flex-end"}}>
+                        Publish
+                    </button>
+                    
+                    </form>
+                    </div>
+            </div>
                 
             
         );
@@ -44,4 +46,3 @@ const NewPost = () => {
         
 
 export default NewPost;
-
