15d27afe657b5c33ef88462333c034789bed5abc
jesuisjayus <aurelien.girin@gmail.com>
2023-06-06 19:45:24 +0200

    handleLogin

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const SignIn = () => {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async(e) => {
+        e.preventDefault();
+        try{
+            const res = await axios.post("http://localhost:8000/api/auth/signin/", {userName, password});
+            console.log("res", res.data);
+        } catch(err){
+            console.log(err);
+        }
+    };
+
     return (
         <div className="mt-10 grid grid-cols-3 gap-20">
             <img src="/logo.png" alt="Logo" width="1000px"/>
@@ -8,9 +22,17 @@ const SignIn = () => {
                 <h2 className="text-3xl text-text font-bold text-center">
                     Sign in to your account
                 </h2>
-                <input type="text" placeholder="Username" className="text-xl px-4 py-2 rounded-full"/>
-                <input type="password" placeholder="Password" className="text-xl px-4 py-2 rounded-full"/>
-                <button className="bg-button text-white text-xl px-4 py-2 rounded-full hover:bg-button-hover">Sign In</button>
+                <input
+                    onChange={(e) => setUserName(e.target.value)}
+                    type="text" 
+                    placeholder="Username" 
+                    className="text-xl px-4 py-2 rounded-full"/>
+                <input
+                    onChange={(e) => setPassword(e.target.value)} 
+                    type="password" 
+                    placeholder="Password" 
+                    className="text-xl px-4 py-2 rounded-full"/>
+                <button className="bg-button text-white text-xl px-4 py-2 rounded-full hover:bg-button-hover" onClick={handleLogin}>Sign In</button>
             </form>
             <form className="bg-form flex flex-col px-8 py-12 rounded-lg mx-auto gap-10">
                 <p className="mx-auto text-text">Don't have any account?</p>
