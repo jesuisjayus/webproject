42ea1a5046106056819a58fda2b3242bab4b28f8
AlexChnll <alexandra.chianelli@insa-lyon.fr>
2023-06-07 09:54:41 +0200

    handle button event

--- client/src/pages/Manage/Manage.jsx
+++ client/src/pages/Manage/Manage.jsx
@@ -5,15 +5,23 @@ import CloseIcon from "@mui/icons-material/Close";
 import { Link } from "react-router-dom";
 import Leftbar from "../../components/ManageLeftbar/ManageLeftbar"; 
 import Navbar from "../../components/Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
 
 const Manage = () => {
+    const [description, setDescription] = useState("");
     const [characterCount, setCharacterCount] = useState(0);
+    const [userName, setUserName] = useState("");
+    const [birthDate, setBirthDate] = useState("");
+    const [selectedImage, setSelectedImage] = useState('');
+    const navigate = useNavigate();
 
     const handleTextareaChange = (event) => {
         const text = event.target.value;
         setCharacterCount(text.length);
+        setDescription(text);
+        console.log(text);
+        
     };
-    const [selectedImage, setSelectedImage] = useState('');
 
     const handleImageChange = (event) => {
       const file = event.target.files[0];
@@ -40,6 +48,24 @@ const Manage = () => {
         objectFit: 'cover',
     };
 
+    const handleBirthDate = (event) => {
+        var regex = /^\d{2}\/\d{2}\/\d{4}$/;
+        if (regex.test(event.target.value)) {
+          // Le format de la date est correct, vous pouvez mettre à jour la variable
+          setBirthDate(event.target.value);
+          console.log(event.target.value);
+        } else {
+          // Afficher un message d'erreur ou prendre d'autres mesures en cas de format incorrect
+          console.log("Format de date incorrect !");
+        }
+    };
+
+    const handleValidate = (event) => {
+        event.preventDefault();;
+        navigate("/profile/:id");
+    }
+
+
 
     return (
         <div>
@@ -58,18 +84,15 @@ const Manage = () => {
                     <div style={profilePictureStyle}>
                         <img src={selectedImage} alt="" style={imageStyle} />
                     </div>
-                    <input type="file" accept=".jpeg, .jpg, .png" onChange={handleImageChange} />
-                        <input type="text" placeholder="Username" className="bg-blue-100 rounded-full py-2 px-2"/>
-                        <input type="text" placeholder="Birth Date (DD/MM/YYYY)" pattern="\d{2}/\d{2}/\d{4}" title="Format DD/MM/YYYY" className="bg-blue-100 rounded-full py-2 px-2" required/>
+                        <input type="file" accept=".jpeg, .jpg, .png" onChange={handleImageChange} />
+                        <input type="text" placeholder="Username" className="bg-blue-100 rounded-full py-2 px-2" onChange={(e) => setUserName(e.target.value)}/>
+                        <input type="text" placeholder="Birth Date (DD/MM/YYYY)" pattern="\d{2}/\d{2}/\d{4}" title="Format DD/MM/YYYY" className="bg-blue-100 rounded-full py-2 px-2" onChange={handleBirthDate} required/>
                         <textarea maxlength={500} title="max 500 characters" placeholder="Write a little description..." className="bg-blue-100 rounded-lg py-2 px-2" onChange={handleTextareaChange}></textarea>
                         <p>number of characters : {characterCount}/500</p>
-                        <div className="grid grid-cols-2 gap-4">
-                            <Link to="/profile/:id">
-                                <div className="bg-button text-white text-xl px-4 py-2 rounded-full hover:bg-button-hover">
-                                    <DoneIcon fontSize="large" />
-                                    <span className="ml-8">Validate</span>
+                        <div className="grid grid-cols-2 gap-1">
+                                <div style={{width:"100%"}}>
+                                    <button className="bg-button resize-none px-4 py-2 text-white rounded-full hover:bg-button-hover" style={{display: "flex", justifyContent:"center", alignItems:"flex-end", width:"60%"}}  onClick={handleValidate}><DoneIcon fontSize="large" style={{ marginRight: "8px" }} />Validate</button>
                                 </div>
-                            </Link>
                             <Link to="/profile/:id">
                                 <div className="bg-red-500 text-white text-xl px-4 py-2 rounded-full hover:bg-red-300">
                                     <CloseIcon fontSize="large" />
--- client/src/pages/NewPost/NewPost.jsx
+++ client/src/pages/NewPost/NewPost.jsx
@@ -4,20 +4,35 @@ import { useState } from 'react';
 import { Link } from "react-router-dom";
 import ChooseCategory from "../../components/ChooseCategory/ChooseCategory";
 import Navbar from "../../components/Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
 
 import SocialMedia from "../../components/SocialMedia/SocialMedia";
 const NewPost = () => {
     const [characterCount, setCharacterCount] = useState(0);  // for short description
     const [characterCount2, setCharacterCount2] = useState(0); // for description
+    const [shortDescription, setShortDescription] = useState("");
+    const [longDescription, setLongDescription] = useState("");
+    const [title, setTitle] = useState("");
+    const navigate = useNavigate();
+
     const handleTextareaChange_short = (event) => {
         const text = event.target.value;
         setCharacterCount(text.length);
-        
+        setShortDescription(text);        
     };
+
     const handleTextareaChange_description = (event) => {
         const text = event.target.value;
         setCharacterCount2(text.length);
+        setLongDescription(text);
     };
+
+    const handlePublish = (event) => {
+        event.preventDefault();
+        console.log(title+""+shortDescription+""+longDescription);
+        navigate("/home");
+    };
+
     return (
         <div>
             <div>
@@ -28,23 +43,23 @@ const NewPost = () => {
                 <Leftbar />
                 </div>
                 <div className="col-span-3 border-t-slate-800">
-                <form className="bg-form flex flex-col px-8 py-12 rounded-lg w-8/12 mx-0 gap-10">
-                    <h2 className="text-3xl text-text font-bold text-center">
-                        New Post
-                    </h2>
-                    <input type="text" placeholder="Title" className="text-xl px-4 py-2 rounded-full"/>
-                    <ChooseCategory /> 
-                    <textarea maxlength={280} title="max 280 characters" placeholder="Write a short description..." className="bg-blue-100 rounded-lg py-2 px-2" onChange={handleTextareaChange_short}></textarea>
-                    <p className="text-text">Number of characters : {characterCount}/280</p>
-                    <textarea maxlength={1000} title="max 1000 characters" placeholder="Write the full description..." className="bg-blue-100 rounded-lg py-2 px-2" onChange={handleTextareaChange_description}></textarea>
-                    <p className="text-text">Number of characters : {characterCount2}/1000</p>
-                    <button class="bg-button resize-none px-4 py-2 text-white rounded-full hover:bg-button-hover" style={{display: "flex", justifyContent:"center", alignItems:"flex-end"}}>
-                        Publish
-                    </button>
-                    
-                    </form>
-                    
-                    </div>
+                    <form className="bg-form flex flex-col px-8 py-12 rounded-lg w-8/12 mx-0 gap-10">
+                        <h2 className="text-3xl text-text font-bold text-center">
+                            New Post
+                        </h2>
+                        <input type="text" placeholder="Title" className="text-xl px-4 py-2 rounded-full" onChange={(e) => setTitle(e.target.value)}/>
+                        <ChooseCategory /> 
+                        <textarea maxlength={280} title="max 280 characters" placeholder="Write a short description..." className="bg-blue-100 rounded-lg py-2 px-2" onChange={handleTextareaChange_short}></textarea>
+                        <p className="text-text">Number of characters : {characterCount}/280</p>
+                        <textarea maxlength={1000} title="max 1000 characters" placeholder="Write the full description..." className="bg-blue-100 rounded-lg py-2 px-2" onChange={handleTextareaChange_description}></textarea>
+                        <p className="text-text">Number of characters : {characterCount2}/1000</p>
+                            <div className="flex justify-center">
+                            <button class="bg-button resize-none px-4 py-2 text-white rounded-full hover:bg-button-hover" style={{display: "flex", justifyContent:"center", alignItems:"flex-end", width:"60%"}} onClick={handlePublish}>
+                                Publish
+                            </button>
+                            </div>
+                    </form>    
+                </div>
                     <a>
                     <span style={{ height:"50px", width: "4rem" }}></span> 
                     <SocialMedia/>
