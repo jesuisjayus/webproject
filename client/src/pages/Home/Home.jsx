90cbb8c6853f010e23d482dc7df905969909f8b7
Nitsugu81 <122277644+Nitsugu81@users.noreply.github.com>
2023-06-08 08:36:01 +0200

    page Home (redux)

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import Leftbar from "../../components/HomeLeftbar/HomeLeftbar";
 import Navbar from "../../components/Navbar/Navbar";
+import SignIn from "../SignIn/SignIn";
+
+import {useSelector } from "react-redux";
+import {useNavigate} from "react-router-dom";
+
 
 const Home = () => {
+    const {currentUser} = useSelector((state) => state.user);
+    console.log(currentUser);
     return (
+
         <>
-            <div>
-                <Navbar />
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-4">
-                <div className="px-6"><Leftbar /></div>
-                <div className="col-span-3 border-t-slate-800 px-6">
-                    <p>Annonce</p>
+            {!currentUser ? (
+                <SignIn/>
+            ) : (
+                <>
+                <div>
+                    <Navbar />
                 </div>
-            </div>
+                <div className="grid grid-cols-1 md:grid-cols-4">
+                    <div className="px-6"><Leftbar /></div>
+                    <div className="col-span-3 border-t-slate-800 px-6">
+                        <p>Annonce</p>
+                    </div>
+                </div>
+                </>
+            )}
         </>
         );
 };
