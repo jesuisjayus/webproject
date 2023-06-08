b49758a4afdb8f927c37019347e8c6742a2fa9ae
AlexChnll <alexandra.chianelli@insa-lyon.fr>
2023-06-08 11:30:11 +0200

    update navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -9,31 +9,36 @@ const Navbar = () =>{
   const handleLogout = () =>{
       dispatch(logout());
     } 
+
   return (
-      <div className="flex justify-between items-center my-5">
-      <div className="mx-auto md:mx-0">
+    <div className="flex flex-wrap justify-between items-center my-5">
+      <div className="flex items-center">
         <img src="/logo.png" alt="Logo" width="40px" className="ml-8" />
+        <h2 className="font-bold text-2xl ml-2">SkillConnect</h2>
       </div>
-      <div className="col-span-2">
-        <div className="flex justify-between items-center">
-          <h2 className="font-bold text-2xl">SkillConnect</h2>
+      <div className="w-full md:w-auto px-4 md:px-6 sm:my-4 mx-auto" style={{ position: 'relative'}}>
+        <SearchIcon className="absolute m-2" />
+        <input type="text" className="bg-blue-100 rounded-full py-2 px-8 w-full md:w-auto md:w-[170%] md:min-w-[10rem] md:left-2" placeholder="Search..."/>
+      </div>
+      <div className="w-full md:w-auto col-span-5 md:my-0 px-6 mx-auto md:flex md:justify-end" style={{ position: 'relative' }}>
+        <div className="md:hidden">
+          <Link to="/">
+            <button className="bg-red-500 px-4 py-2 text-white rounded-full" onClick={handleLogout}>
+              Logout
+            </button>
+          </Link>
         </div>
       </div>
-      <div className="col-span-3 md:border-x-2 md:border-slate-200 px-0 md:px-6 mx-auto" style={{ position: 'relative' }}>
-        <SearchIcon className="absolute m-2" />
-        <input type="text" className="bg-blue-100 rounded-full py-2 px-8" style={{ width: '170%', left: '2rem' }} placeholder="Search..." />
-      </div>
-      <div className="col-span-5 md:border-x-2 md:border-slate-200 md:my-0 px-6 mx-auto flex-end" style={{ position: 'relative' }}>
+      <div className="hidden md:w-auto col-span-5 md:my-0 px-6 mx-auto md:flex md:justify-end" style={{ position: 'relative' }}>
         <div>
           <Link to="/">
             <button className="bg-red-500 px-4 py-2 text-white rounded-full" onClick={handleLogout}>
-                Logout
+              Logout
             </button>
           </Link>
         </div>
       </div>
     </div>
-      
 
     );
 
