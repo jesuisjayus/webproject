a512f4c3bb7c5459d31dc16c79cdd4f9fab057d6
Alex Baciu <baciu6657@gmail.com>
2023-06-09 17:30:50 +0300

    DarkMode

---
+++ client/src/components/Chat/SignInCh.js
@@ -0,0 +1,23 @@
+
+/*import React from 'react'
+import {Button} from '@mui/material'
+import firebase from 'firebase'
+import { auth} from './firebase'
+
+
+function SignInCh(){
+    function signInWithGoogle()
+        {
+         const provider=new firebase.auth.signInWithGoogleAuthProvider();
+          auth.signInWithPopup(provider);
+        }
+    return (
+        <div>
+        <Button onClick={signInWithGoogle}>Sign In With Google</Button>
+
+        </div>
+    )
+}
+
+export default SignInCh
+*/
