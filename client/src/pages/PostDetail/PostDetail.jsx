66063aa1c95372c0ce974bc80cc0ced3a5579b55
jesuisjayus <aurelien.girin@gmail.com>
2023-06-14 09:25:51 +0200

    updated postdetail

--- client/src/pages/PostDetail/PostDetail.jsx
+++ client/src/pages/PostDetail/PostDetail.jsx
@@ -36,28 +36,26 @@ const PostDetail = () => {
                         <Leftbar />
                     </div>
                     <div className="col-span-3 border-t-slate-800 px-6">
-                        <form className="bg-gradient-to-bl from-form-pink via-form-purple to-form-blue flex flex-col px-8 py-12 rounded-lg w-8/12 mx-0 gap-5">
+                        <form className="bg-gradient-to-b from-form-pink to-form-purple flex flex-col px-8 py-12 rounded-lg w-8/12 mx-0 gap-5">
                                 <h2 className="text-3xl text-text font-bold text-center rounded">
                                     Post Details
                                 </h2> 
                             <div className="bg-blue-100 text-xl px-4 py-2 rounded-full">
-                                <h1><b> </b> {state.title}</h1>
+                                <h1><b>Announce : </b> {state.title}</h1>
                             </div>
                             <div className="bg-blue-100 text-xl px-4 py-2 rounded-full">
-                                <h1><b> </b> {state.category}</h1>
+                                <h1><b>Category : </b> {state.category}</h1>
                             </div>
-                            <div className="bg-blue-100 text-xl px-4 py-2 rounded-full">
-                                <h1><b> </b> {state.text}</h1>
+                            <div className="bg-blue-100 text-xl px-4 py-2 rounded-lg text-justify">
+                                <h1><b>Description : </b> {state.description}</h1>
                             </div>
-                            <div className="bg-blue-100 text-xl px-4 py-2 rounded-full">
-                                <h1><b> </b> {state.description}</h1>
-                            </div>
+                        </form>
+                        <form className="bg-gradient-to-b from-form-purple to-form-blue flex flex-col px-8 py-12 rounded-lg w-8/12 mx-0 my-5 gap-5">
                             <h1 className="text-3xl font-bold mb-4">Qui suis-je?</h1>
-
                             <div className="bg-blue-100 text-xl px-4 py-2 rounded-lg">
                                 <h1><b>UserName : </b> {userData.userName}</h1>
-                                <h1><b>Last Name : </b> {userData.firstName}</h1>
-                                <h1><b>First Name : </b> {userData.lastName}</h1>
+                                <h1><b>First Name : </b> {userData.firstName}</h1>
+                                <h1><b>Last Name : </b> {userData.lastName}</h1>
                             </div>
                             <div className="bg-blue-100 text-xl px-4 py-2 rounded-full">
                                 <h1><b>Email : </b> {userData.email}</h1>
