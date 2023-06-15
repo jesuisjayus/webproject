aa313b7b508c4f3930bddd65e669bfb3a7ad7e53
baptistegrr <122107948+baptistegrr@users.noreply.github.com>
2023-06-15 08:29:41 +0200

    correct profile user.otherDatas

--- client/src/components/TimeLinePostProfile/TimeLinePostProfile.jsx
+++ client/src/components/TimeLinePostProfile/TimeLinePostProfile.jsx
@@ -20,13 +20,13 @@ const TimeLinePostProfile = () => {
             }
         };
         fetchData();
-    }, [currentUser._id]);
+    }, [currentUser.otherDatas._id]);
     
     return (
         <div className="mt-2">
           {timeLine &&
             timeLine.map((post) => {
-              if(post.userId === currentUser._id){
+              if(post.userId === currentUser.otherDatas._id){
                 return (
                   <div key={post._id}>
                       <PostProfile post = {post} setData={setTimeLine} />
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -22,7 +22,7 @@ const Profile = () => {
             }
         };
         fetchData();
-    }, [currentUser._id]);
+    }, [currentUser.otherDatas._id]);
 
     return (
         <div>
@@ -39,20 +39,20 @@ const Profile = () => {
                                 Your Profile
                             </h2> 
                         <div className="bg-blue-100 text-xl px-4 py-2 rounded-full">
-                            <h1><b>User Name : </b> {currentUser.userName}</h1>
+                            <h1><b>User Name : </b> {currentUser.otherDatas.userName}</h1>
                         </div>
                         <div className="bg-blue-100 text-xl px-4 py-2 rounded-full">
-                            <h1><b>Last Name : </b> {currentUser.firstName}</h1>
-                            <h1><b>First Name : </b> {currentUser.lastName}</h1>
+                            <h1><b>Last Name : </b> {currentUser.otherDatas.firstName}</h1>
+                            <h1><b>First Name : </b> {currentUser.otherDatas.lastName}</h1>
                         </div>
                         <div className="bg-blue-100 text-xl px-4 py-2 rounded-full">
-                            <h1><b>Email : </b> {currentUser.email}</h1>
+                            <h1><b>Email : </b> {currentUser.otherDatas.email}</h1>
                         </div>
                         <div className="bg-blue-100 text-xl px-4 py-2 rounded-full">
-                            <h1><b>Birth Date : </b> {currentUser.birthDate}</h1>
+                            <h1><b>Birth Date : </b> {currentUser.otherDatas.birthDate}</h1>
                         </div>
                         <div className="bg-blue-100 text-xl px-4 py-2 rounded-lg text-justify">
-                            <h1><b>Description : </b> {currentUser.description}</h1>
+                            <h1><b>Description : </b> {currentUser.otherDatas.description}</h1>
                         </div>
                     </form>
                     <div className="flex flex-col w-8/12">
